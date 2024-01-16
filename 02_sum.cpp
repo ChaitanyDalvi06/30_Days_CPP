480e83d170e59e371801bbc57e404a701a617da2
Chaitanya Dalvi <142775927+ChaitanyDalvi06@users.noreply.github.com>
2024-01-16 10:02:29 +0530

    Add files via upload

---
+++ 02_sum.cpp
@@ -0,0 +1,13 @@
+//Create a program that takes two numbers as input and calculates their sum.
+#include <iostream>
+using namespace std;
+int main()
+{
+int num1,num2,result;
+cout<<"Enter the number 1 : ";
+cin>>num1;
+cout<<"Enter the number 2 : ";
+cin>>num2;
+result=num1+num2;
+cout<<"The sum of "<< num1 <<" and "<< num2 <<" is : "<<result<<endl;
+}
